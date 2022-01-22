3a67f521f7b0c4d88d077ea89844b0b2808620b7
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2022-01-22 20:31:30 +0530

    swagger ui as homepage

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ application.config.update({
         openapi_version='2.0.0'
     ),
     'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
-    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+    'APISPEC_SWAGGER_UI_URL': '/'  # URI to access UI of API Doc
 })
 docs = FlaskApiSpec(application)
 
